eedfb78b135417bc7aaab9b383bf4efd25c8eef8
Berge-Repositories <147892767+Berge-Repositories@users.noreply.github.com>
2024-10-23 16:37:18 -0400

    Add files via upload

---
+++ WarpSploit/WindowsFormsApp1/AccentColorHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+public class AccentColorHelper
+{
+    [DllImport("dwmapi.dll", EntryPoint = "DwmGetColorizationColor")]
+    public static extern void DwmGetColorizationColor(out uint color, out bool opaque);
+
+    public static Color GetAccentColor()
+    {
+        uint color;
+        bool opaque;
+        DwmGetColorizationColor(out color, out opaque);
+
+        // Extract the RGB values from the color
+        byte red = (byte)((color >> 16) & 0xFF);
+        byte green = (byte)((color >> 8) & 0xFF);
+        byte blue = (byte)(color & 0xFF);
+
+        return Color.FromArgb(red, green, blue);
+    }
+}
